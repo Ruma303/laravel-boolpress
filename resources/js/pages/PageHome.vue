7b0c46906a07ed604498001abfffda9370e9fdec
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-02-07 18:52:35 +0100

    homepage (da aggiornare)

--- resources/js/pages/PageHome.vue
+++ resources/js/pages/PageHome.vue
@@ -1,13 +1,13 @@
+<!-- <router-link :to="{name: 'postsShow', params:{slug: post.slug}}"> -->
+<!-- :src="'/storage/' + post.uploaded_img" -->
 <template>
-    <!-- <h1>Home</h1> -->
     <div class="grid">
-        <div v-for="post in arrRandom" :key="post.id">
-            <router-link :to="{name: 'postsShow', params:{slug: post.slug}}">
-                <!-- :src="'/storage/' + post.uploaded_img" -->
-                <img
-                :src="posts.image"
-                :alt="post.title">
-            </router-link>
+        <div class="tile">
+            <div v-for="post in arrRandom" :key="post.id">
+                <router-link :to="{name: 'postsShow', params: {slug: post.slug}}">
+                    <img :src="post.image" :alt="post.title">
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -27,6 +27,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.grid {
+    display: flex;
+    flex-wrap: wrap;
+    height: 70vh;
+.tile {
+    flex: 0 0 calc(100% / 3);
+    height: calc(100% / 3);
+    img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    }
+    }
+}
 </style>
